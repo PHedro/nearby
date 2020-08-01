585f48232e4994a5e6bdd38391fd3b46e70dcab4
PHedro <pedrohenrique.reis@gmail.com>
2020-08-01 16:29:42 +0100

    calculate distance using haversine

--- nearby/geolocation.py
+++ nearby/geolocation.py
@@ -1,15 +1,33 @@
-import math
+from math import radians, sin, cos, asin, sqrt
 
 
 class GeoLocation:
+    EARTH_RADIUS_KM = 6371
+    EARTH_RADIUS_MILES = 3956
+
     def __init__(self, latitude, longitude):
         self.latitude = float(latitude)
         self.longitude = float(longitude)
-        self.latitude_rad = math.radians(self.latitude)
-        self.longitude_rad = math.radians(self.longitude)
+        self.latitude_rad = radians(self.latitude)
+        self.longitude_rad = radians(self.longitude)
 
     def __repr__(self):
         return "GeoLocation Lat: %r, Long: %r" % (self.latitude, self.longitude)
 
-    def distance_to(self, other_geolocation):
-        pass
+    def distance_to(self, other, use_metric=True):
+        sin_half_delta_lat = sin((other.latitude_rad - self.latitude_rad) / 2)
+        sin_half_delta_long = sin((other.longitude_rad - self.longitude_rad) / 2)
+
+        cos_self_lat = cos(self.latitude_rad)
+        cos_other_lat = cos(other.latitude_rad)
+
+        haversine = 2 * asin(
+            sqrt(
+                sin_half_delta_lat ** 2
+                + cos_self_lat * cos_other_lat * sin_half_delta_long ** 2
+            )
+        )
+
+        multiplier = self.EARTH_RADIUS_KM if use_metric else self.EARTH_RADIUS_MILES
+
+        return haversine * multiplier
---
+++ nearby/tests/test_distance_calculator.py
@@ -0,0 +1,49 @@
+import unittest
+
+from nearby.geolocation import GeoLocation
+
+
+class DistanceCalculatorTestCase(unittest.TestCase):
+    def test_distance_in_km_correctly_2886_444442837984(self):
+        destination = GeoLocation(latitude=36.12, longitude=-86.67)
+        origin = GeoLocation(latitude=33.94, longitude=-118.40)
+        expected = 2886.444442837984
+
+        result = origin.distance_to(destination)
+        self.assertEqual(expected, result)
+
+    def test_distance_in_miles_correctly_1792_3048525925387(self):
+        destination = GeoLocation(latitude=36.12, longitude=-86.67)
+        origin = GeoLocation(latitude=33.94, longitude=-118.40)
+        expected = 1792.3048525925387
+
+        result = origin.distance_to(destination, use_metric=False)
+        self.assertEqual(expected, result)
+
+    def test_distance_in_km_correctly_41_768725500836176(self):
+        destination = GeoLocation(latitude=53.339428, longitude=-6.257664)
+        origin = GeoLocation(latitude=52.986375, longitude=-6.043701)
+        expected = 41.768725500836176
+
+        result = origin.distance_to(destination)
+        self.assertEqual(expected, result)
+
+    def test_distance_in_km_correctly_264_6531541515143(self):
+        destination = GeoLocation(latitude=55.033, longitude=-8.112)
+        origin = GeoLocation(latitude=52.986375, longitude=-6.043701)
+        expected = 264.6531541515143
+
+        result = origin.distance_to(destination)
+        self.assertEqual(expected, result)
+
+    def test_distance_in_km_correctly_278_4581750754194(self):
+        destination = GeoLocation(latitude=52.2296756, longitude=21.0122287)
+        origin = GeoLocation(latitude=52.406374, longitude=16.9251681)
+        expected = 278.4581750754194
+
+        result = origin.distance_to(destination)
+        self.assertEqual(expected, result)
+
+
+if __name__ == "__main__":
+    unittest.main()

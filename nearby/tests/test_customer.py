8ba41a9d12b85cd28a024f1db2cbf1ce56e5bfbf
PHedro <pedrohenrique.reis@gmail.com>
2020-08-01 17:07:14 +0100

    tests for in_range_from

--- nearby/tests/test_customer.py
+++ nearby/tests/test_customer.py
@@ -1,6 +1,7 @@
 import unittest
 
 from nearby.customer import Customer
+from nearby.geolocation import GeoLocation
 
 
 class CustomerTestCase(unittest.TestCase):
@@ -90,6 +91,79 @@ class CustomerTestCase(unittest.TestCase):
                 latitude="-6.043701",
                 longitude=None,
             )
+
+
+class CustomersInRangeTestCase(unittest.TestCase):
+    def setUp(self):
+        self.origin = GeoLocation(latitude=52.986375, longitude=-6.043701)
+
+    def test_return_correctly_range100(self):
+        expected = [
+            (4, "Ian Kehoe"),
+            (5, "Nora Dempsey"),
+            (6, "Theresa Enright"),
+            (11, "Richard Finnegan"),
+            (12, "Christina McArdle"),
+            (13, "Olive Ahearn"),
+            (15, "Michael Ahearn"),
+            (31, "Alan Behan"),
+            (39, "Lisa Ahearn"),
+        ]
+        result = Customer.in_range_from(
+            origin=self.origin, source_file_path="data/customers.txt", distance_km=100
+        )
+        self.assertEqual(expected, result)
+
+    def test_return_correctly_range30(self):
+        expected = [
+            (5, "Nora Dempsey"),
+            (6, "Theresa Enright"),
+            (11, "Richard Finnegan"),
+            (12, "Christina McArdle"),
+            (15, "Michael Ahearn"),
+            (39, "Lisa Ahearn"),
+        ]
+        result = Customer.in_range_from(
+            origin=self.origin, source_file_path="data/customers.txt", distance_km=30
+        )
+        self.assertEqual(expected, result)
+
+    def test_return_correctly_range15(self):
+        expected = [
+            (11, "Richard Finnegan"),
+            (12, "Christina McArdle"),
+        ]
+        result = Customer.in_range_from(
+            origin=self.origin, source_file_path="data/customers.txt", distance_km=15
+        )
+        self.assertEqual(expected, result)
+
+    def test_return_correctly_range150(self):
+        expected = [
+            (4, "Ian Kehoe"),
+            (5, "Nora Dempsey"),
+            (6, "Theresa Enright"),
+            (8, "Eoin Ahearn"),
+            (9, "Jack Dempsey"),
+            (10, "Georgina Gallagher"),
+            (11, "Richard Finnegan"),
+            (12, "Christina McArdle"),
+            (13, "Olive Ahearn"),
+            (15, "Michael Ahearn"),
+            (17, "Patricia Cahill"),
+            (23, "Eoin Gallagher"),
+            (24, "Rose Enright"),
+            (26, "Stephen McArdle"),
+            (28, "Charlie Halligan"),
+            (29, "Oliver Ahearn"),
+            (30, "Nick Enright"),
+            (31, "Alan Behan"),
+            (39, "Lisa Ahearn"),
+        ]
+        result = Customer.in_range_from(
+            origin=self.origin, source_file_path="data/customers.txt", distance_km=150
+        )
+        self.assertEqual(expected, result)
 
 
 if __name__ == "__main__":
